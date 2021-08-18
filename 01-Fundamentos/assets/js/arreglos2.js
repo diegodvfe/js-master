4b3c1175e9f062e96e65dbea27fab3a5c9ed752e
Diego <diego.lopez.19@hotmail.es>
2021-08-17 22:28:46 -0600

    se creo una nueva variable

--- 01-Fundamentos/assets/js/arreglos2.js
+++ 01-Fundamentos/assets/js/arreglos2.js
@@ -4,8 +4,8 @@ let juegos = ['Mario', 'Zelda', 'Chrono', 'thanos'];
 console.log('Largo:', juego.length);
 
 
-let primero = juego[0];
-let ultimo = juego[juego.length - 1];
+let primero = juegos[0];
+let ultimo = juegos[juego.length - 1];
 
 console.log({primero, ultimo});
 
@@ -13,7 +13,23 @@ juego.forEach( (elemento, indice, arr)=>{
   console.log({elemento, indice, arr})
 } );
 
-let nuevaLongitud = juego.push('Eeto');
+let nuevaLongitud = juegos.push('Eeto');
 console.log({nuevaLongitud, juego});
 
-juego.unshift()
+nuevaLongitud = juegos.unshift('Fire Emble');
+console.log( nuevaLongitud, juego);
+
+let juegoBorrado = juegos.pop()
+console.log(juegoBorrado, juego);
+
+let pos = 1;
+
+juegos.splice( pos, 1);
+
+// class ThreeNode {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
