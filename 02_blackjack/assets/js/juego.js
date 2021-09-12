99d7efbe4d257df0731646c9d9b080c5e0ebe838
Diego <diego.lopez.19@hotmail.es>
2021-09-11 18:33:51 -0600

    blackajck

--- 02_blackjack/assets/js/juego.js
+++ 02_blackjack/assets/js/juego.js
@@ -9,6 +9,18 @@
  const tipos = ['C', 'D', 'H', 'S' ];
  const especiales = [ 'A', 'J', 'Q', 'K'];
 
+ let puntosJugadores     = 0;
+ let puntosCoumputadora  = 0;
+
+//   Referencias de HTML
+const btnPedir = document.querySelector(`#btnPedir`);
+
+const divCartasJugador = document.querySelector('#jugador-cartas');
+const divCartasComputadora = document.querySelector( `#compu-cartas`)
+const points = document.querySelectorAll('small');
+
+
+ // Esta funcion crea una nueva baraja.
  const crearDeck = () =>{
 
     for ( let i = 2; i <=10; i++){
@@ -24,11 +36,95 @@
       }
     // en esta parte se retorna toda la funcion
 
-      console.log( deck);
+      // console.log( deck);
       deck = _.shuffle(deck);
-      console.log( deck );
+      // console.log( deck );
       return deck;
 
  }
 
  crearDeck();
+
+//  Esta funcion es para pedir carta
+
+const pedirCarta = () =>{
+
+  if ( deck.length === 0){
+      throw 'No hay carta en el deck'; // throw es la parte mas complicada
+  }
+
+
+   const carta = deck.shift();
+  //  console.log( carta);
+
+  return carta;
+}
+
+for ( let i; i >=57; i++){
+  pedirCarta();
+}
+
+//  El valor de la cartas
+const valorCarta = ( carta) =>{
+
+    const valor = carta.substring( 0, carta.length -1);
+    // let puntos = 0;
+    // 2 = 2 10 =10
+
+  // isNanN = is not a number
+    return (isNaN(valor) ) ?
+            (valor === 'A') ? 11 : 10
+            : valor *1;
+
+}
+
+//  Turno de la Computadora
+const turnoComputadora = ( puntosMinimos) =>{
+
+    do {
+      const carta = pedirCarta();
+
+      puntosJugadores = puntosJugadores + valorCarta(carta);
+      points[0].innerText = puntosJugadores;
+      console.log(points)
+
+      const imgCartas = document.createElement(`img`);
+      imgCartas.src = `assets/cartas/${carta}.png`;
+      imgCartas.classList.add(`carta`);
+      divCartasJugador.append(imgCartas);
+
+    }while(puntosMinimos);
+
+}
+
+
+// Eventos Pedir botones
+
+btnPedir.addEventListener (`click`, () =>{
+
+  const carta = pedirCarta();
+
+  puntosJugadores = puntosJugadores + valorCarta( carta);
+  points[0].innerText = puntosJugadores;
+  console.log( points)
+
+  const imgCartas = document.createElement(`img`);
+  imgCartas.src = `assets/cartas/${carta}.png`;
+  imgCartas.classList.add( `carta`);
+  divCartasJugador.append( imgCartas);
+
+  if ( puntosJugadores >21){
+    console.warn(`Haz perdido amigo`);
+    btnPedir.disabled = true;
+
+  }else if ( puntosJugadores === 21){
+    console.warn('21 Ganaste')
+    btnPedir.disabled = true;
+  }
+
+});
+
+
+
+
+
